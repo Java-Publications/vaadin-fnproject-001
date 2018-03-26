a03dfd4164b3c1d76c994c6e566867ea9beb9160
Sven Ruppert <sven.ruppert@gmail.com>
2018-03-26 16:56:46 +0200

    init

---
+++ src/main/java/org/rapidpm/fnproject/helloworld/HelloFunction.java
@@ -0,0 +1,9 @@
+package org.rapidpm.fnproject.helloworld;
+
+public class HelloFunction {
+
+    public String handleRequest(String input) {
+        String name = (input == null || input.isEmpty()) ? "world"  : input;
+        return "Hello, " + name + "!";
+    }
+}
---
+++ src/test/java/junit/org/rapidpm/fnproject/helloworld/HelloFunctionTest.java
@@ -0,0 +1,23 @@
+package junit.org.rapidpm.fnproject.helloworld;
+
+import com.fnproject.fn.testing.*;
+import org.junit.*;
+import org.rapidpm.fnproject.helloworld.HelloFunction;
+
+import static org.junit.Assert.*;
+
+public class HelloFunctionTest {
+
+    @Rule
+    public final FnTestingRule testing = FnTestingRule.createDefault();
+
+    @Test
+    public void shouldReturnGreeting() {
+        testing.givenEvent().enqueue();
+        testing.thenRun(HelloFunction.class, "handleRequest");
+
+        FnResult result = testing.getOnlyResult();
+        assertEquals("Hello, world!", result.getBodyAsString());
+    }
+
+}
